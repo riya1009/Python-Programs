193556331e1cc2ab55d4dabb7f6038650863c5ce
RIYA BANSAL <srbiiyam@gmail.com>
2024-01-28 19:03:31 +0530

    Add files via upload

---
+++ functionarguments.py
@@ -0,0 +1,12 @@
+# function tuple can be passed as dictionary or tuple...
+def average(a,b):
+    print("the average is",(a+b)/2)
+
+average(4,6)      
+
+# default arguments
+
+def sum(a=8,b=9):
+    print(a+b)
+
+
---
+++ functions.py
@@ -0,0 +1,22 @@
+def calculateGmean(a,b):
+    mean = (a*b)/(a+b)
+    print(mean)
+
+def isGreater(a,b):
+    if(a>b):
+        print("a is greater")
+    else:
+        print("b is greater")
+
+def isLesser(a,b):
+    pass              # used to tell the compiler that we will define function body afterwards
+a = 9
+b = 8
+isGreater(a,b)
+calculateGmean(a,b)
+
+# gmean1 = (a*b)/(a+b)
+# c = 8
+# d = 7
+# gmean2 = (c*d)/(c+d)
+# built in functiond me def use nhi hota...
