cb06744ef63e1f06f987e3bf9c1c70bcb0e4fa9e
RIYA BANSAL <srbiiyam@gmail.com>
2024-01-29 10:09:00 +0530

    Add files via upload

---
+++ ifelif.py
@@ -0,0 +1,9 @@
+num = int(input("enter the value of num:"))
+if(num<0):
+    print("number is negative")
+elif(num==0):
+    print("number is zero")
+elif(num>0):
+    print("number is positive")
+
+print("i m happy now")
---
+++ ifelse.py
@@ -0,0 +1,12 @@
+a = int(input("enter your age"))
+print("your age is:",a)
+# conditional operators
+print(a>18)
+print(a<=18)
+print(a==18)
+print(a!=18)
+if(a>18):                           # python me curly braces nhi use hota,sirf colon and indentation se btate hai ki kisi blockke nadr hai 
+    print("you can drive")
+else:
+    print("you can't drive")
+    
