b23422d87f38c308beedea6eeeaebce6787961e5
RIYA BANSAL <srbiiyam@gmail.com>
2024-03-01 21:05:47 +0530

    Add files via upload

---
+++ userinput.py
@@ -0,0 +1,10 @@
+# to take user input we use input function
+# a = input()
+# print("my name is",a)
+
+# when we input number it is by default entered as string:
+
+x = input("enter first number")
+y = input("enter second number")
+print(x+y)
+# apply implicit typecasting here to convert string into number to perform appropriate operations.
