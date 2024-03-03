1f0540de25d7b479e8acc9236fa86929b6e8479c
RIYA BANSAL <srbiiyam@gmail.com>
2024-03-03 21:33:58 +0530

    Add files via upload

---
+++ typecasting.py
@@ -0,0 +1,17 @@
+# a="1"
+# b="2"
+# a1 = 1
+# b1 = 2
+# print(a+b)
+# print(a1+b1)
+# print(int(a)+int(b))
+# example of explicit conversion
+# string = "15"
+# number = 7
+# string_number = int(string) #throws an error if the string is not an valid integer
+# sum = number + string_number
+# print("the sum of both the number is:",sum)
+# implicit type casting
+c=1.9
+d=8
+print(c+d)
